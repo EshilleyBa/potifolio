3baac5dab4560cc70f7c8cca52b8ea90d2bd2008
Eshilley Hiury Minicucci Vianna Barbosa <eshilleyhmvb@gmail.com>
2022-12-12 07:59:51 -0300

    fixes

--- src/app.py
+++ src/app.py
@@ -1,19 +1,19 @@
 from flask import app, Flask, render_template
 
-jooj = Flask(__name__)
+app = Flask(__name__)
 
-@jooj.route('/')
+@app.route('/')
 def home():
     return render_template('index.html')
 
-@jooj.route('/projetos')
+@app.route('/projetos')
 def projetos():
     return render_template('projetos.html')
 
     
-@jooj.route('/mais')
+@app.route('/mais')
 def mais():
     return render_template('mais.html')
 
 if __name__ == '__main__':
-    jooj.run('0.0.0.0')
+    app.run('debug=True')
